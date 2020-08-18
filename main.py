ca1e25c4dee7e4bdb55fc113547354f17c512bdc
AlexMnrs <alexmnrs92@gmail.com>
2020-08-18 11:51:19 +0200

    Diferenciación de caracteres

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import string
 import random
 
-def gen_pass(size = 8, 
-chars = string.ascii_letters + 
-string.digits + 
+def gen_pass(size = 8,
+chars = string.ascii_letters +
+string.digits +
 string.punctuation):
 	return ''.join(random.choice(chars) for _ in range(size))
-	
+
 if __name__ == "__main__":
 	while True:
 		size = input("Cuántos caracteres debe tener la contraseña? (Por defecto: 8) ")
-		
-		if not size.isdigit() and size == '':
+
+		if size == '':
 			password = gen_pass()
 			break
+		elif not size.isdigit():
+			print("El caracter introducido no es un número.")
 		elif size != '' and size.isdigit():
 			size = int(size)
 			password = gen_pass(size)
 			break
-	
+
 	print(password)
